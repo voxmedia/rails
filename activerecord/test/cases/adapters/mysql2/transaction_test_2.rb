0d479a74cc48ee0945a1e65b233cdf43432cca33
Jamie McCarthy <jamie.mccarthy@voxmedia.com>
2017-09-28 22:08:42 +0200

    Fix up tests

--- activerecord/test/cases/adapters/mysql2/transaction_test_2.rb
+++ activerecord/test/cases/adapters/mysql2/transaction_test_2.rb
@@ -33,7 +33,35 @@ module ActiveRecord
       Thread.abort_on_exception = @abort
     end
 
-    test "rolls back with no exception after Deadlocked inside SavepointTransaction" do
+    test "deadlock correctly raises Deadlocked inside RealTransaction" do
+      barrier = Concurrent::CyclicBarrier.new(2)
+      s1 = Sample.create value: 1
+      s2 = Sample.create value: 2
+
+      assert_raises(ActiveRecord::Deadlocked) do
+        thread = Thread.new do
+          # Start a RealTransaction
+          Sample.transaction(:requires_new => false) do
+            s1.lock!
+            barrier.wait
+            s2.update_attributes value: 1
+          end
+        end
+
+        begin
+          # Start a RealTransaction
+          Sample.transaction(:requires_new => false) do
+            s2.lock!
+            barrier.wait
+            s1.update_attributes value: 2
+          end
+        ensure
+          thread.join
+        end
+      end
+    end
+
+    test "deadlock correctly raises Deadlocked inside nested SavepointTransaction" do
       barrier = Concurrent::CyclicBarrier.new(2)
       s1 = Sample.create value: 1
       s2 = Sample.create value: 2
@@ -74,9 +102,50 @@ module ActiveRecord
       end
     end
 
-    test "raises TransactionTimeout when mysql raises ER_LOCK_WAIT_TIMEOUT" do
-      assert_raises(ActiveRecord::TransactionTimeout) do
-        ActiveRecord::Base.connection.execute("SIGNAL SQLSTATE 'HY000' SET MESSAGE_TEXT = 'Testing error', MYSQL_ERRNO = 1205;")
+    test "deadlock correctly raises Deadlocked inside double-nested SavepointTransaction" do
+      barrier = Concurrent::CyclicBarrier.new(2)
+      s1 = Sample.create value: 1
+      s2 = Sample.create value: 2
+
+      begin
+        thread = Thread.new do
+          # Start a RealTransaction
+          Sample.transaction(:requires_new => false) do
+            # Start a SavepointTransaction inside it
+            Sample.transaction(:requires_new => true) do
+              # Start another SavepointTransaction
+              Sample.transaction(:requires_new => true) do
+                s1.lock!
+                barrier.wait
+                s2.update_attributes value: 1
+              end
+            end
+          end
+        end
+
+        begin
+          # Start a RealTransaction
+          Sample.transaction(:requires_new => false) do
+            # Start a SavepointTransaction inside it
+            Sample.transaction(:requires_new => true) do
+              # Start another SavepointTransaction
+              Sample.transaction(:requires_new => true) do
+                s2.lock!
+                barrier.wait
+                s1.update_attributes value: 2
+              end
+            end
+          end
+        ensure
+          thread.join
+        end
+
+      rescue ActiveRecord::StatementInvalid => e
+        if /SAVEPOINT active_record_. does not exist: ROLLBACK TO SAVEPOINT/ =~ e.to_s
+          assert nil, 'ROLLBACK TO SAVEPOINT query issued for savepoint that no longer exists due to deadlock'
+        else
+          raise e
+        end
       end
     end
   end
